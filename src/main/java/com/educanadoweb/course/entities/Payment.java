11bb42f476b242ebd280f01f43d524a4c7e4a8af
Johnnatan Lopes <johnnatandev9@gmail.com>
2024-12-31 07:59:38 -0300

    Subtotal & Total methods

--- src/main/java/com/educanadoweb/course/entities/Payment.java
+++ src/main/java/com/educanadoweb/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.educanadoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import org.aspectj.weaver.ast.Or;
 
@@ -16,6 +17,7 @@ public class Payment implements Serializable{
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
